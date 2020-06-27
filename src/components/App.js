6445c968438bbe9a046d0d41887cf9c28677afd8
devmobin <mobinheydari114@gmail.com>
2020-06-27 10:26:36 +0430

    posts list component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import PostList from './PostList';
 
 function App() {
   return (
     <div className="ui container">
-      app
+      <PostList />
     </div>
   );
 }

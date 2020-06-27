ac55c3bc7012ca58f919a0c2aaba89f30afec422
devmobin <mobinheydari114@gmail.com>
2020-06-27 23:28:04 +0430

    get the state into the component and render list

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux'
+import postsReducer from './postsReducer'
 
 export default combineReducers({
-  replaceMe: () => 'state will be added',
+  posts: postsReducer,
 })
